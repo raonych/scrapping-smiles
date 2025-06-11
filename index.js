a7cbe387f7f89ad83bce00efab506b651e77db0c
raonych <raonych@gmail.com>
2025-06-11 15:29:06 -0300

    feat: passando pelo filtro de companhia

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ const fs = require("fs");
     const month = new Date().getUTCMonth()
     const year = new Date().getFullYear()
     const formatedDate = day < 10? `date_0${day}${month+1}${year}` : `date_${day}${month+1}${year}`;
-    const flightsData = [];
-    var count = 0;
+    var count = 1;
 
     let options = new firefox.Options();
 
     options.setPreference('dom.webnotifications.enabled', false);
     options.setPreference('permissions.default.geo', 2);
-
+    options.addArguments("-private");
 
     let driver = await new Builder()
         .forBrowser(Browser.FIREFOX)
@@ -26,12 +25,12 @@ const fs = require("fs");
     await driver.manage().window().maximize();
 
     await new Promise((r)=> setTimeout(r, 5000));
-    acceptCookie = await driver.wait(until.elementLocated(By.id("onetrust-accept-btn-handler")), 10000);
+    const acceptCookie = await driver.wait(until.elementLocated(By.id("onetrust-accept-btn-handler")), 10000);
     await driver.wait(until.elementIsVisible(acceptCookie),5000);
     await driver.wait(until.elementIsEnabled(acceptCookie),5000);
     await acceptCookie.click();
 
-    origem = await driver.findElement(By.id("inp_flightOrigin_1"))
+    const origem = await driver.findElement(By.id("inp_flightOrigin_1"))
     await driver.wait(until.elementIsEnabled(origem),5000);
     origem.click();
     origem.sendKeys("Guarulhos");
@@ -59,7 +58,7 @@ const fs = require("fs");
     
     try{
         await driver.wait(until.elementLocated(By.css(".smls-flight-search-default-modal.modal.fade.show.redesign.backdrop.trap")), 5000).click();       
-        btnSameDay = await driver.wait(until.elementLocated(By.id("btn_sameDayInternational")), 5000);
+        const btnSameDay = await driver.wait(until.elementLocated(By.id("btn_sameDayInternational")), 5000);
         await driver.wait(until.elementIsVisible(btnSameDay),5000);
         btnSameDay.click();
     
@@ -69,6 +68,7 @@ const fs = require("fs");
     }
     
     while (count <= 10){
+        const flightsData = [];
         console.log("Esperando carregamento da pagina de voos...")      
         //verifica se página de voos ainda esta carregando
         while(true){
@@ -82,35 +82,91 @@ const fs = require("fs");
         
 
         //verifica se tem voos disponiveis na página
-        try{
-            const noFlightsElement = await driver.wait(
+        try {
+        const noFlightsElement = await driver.wait(
             until.elementLocated(By.className("select-flight-not-found-card")),
             10000
-            );
-            await driver.wait(until.elementIsVisible(noFlightsElement), 5000); 
-            console.log("sem voos disponiveis neste dia");
-            count ++;
-            await passDay(driver);
-            continue;
-            
-        }catch(error){
-            console.log("Voo disponível");
+        );
+        await driver.wait(until.elementIsVisible(noFlightsElement), 5000);
+        
+        console.log("Sem voos disponíveis .");
+        count++;
+        await passDay(driver);
+        continue;
         }
 
-        while(true){
-            try{
-                const moreFlights = await driver.wait(until.elementLocated(By.id('SelectFlightList-ida-more')), 10000);
-                moreFlights.click();
-                break;
+        catch (error) {
+            console.log("Voos disponíveis. Continuando com a extração...");
+        }
+
+
+        let data = await scrollAndSearch(driver);
+        flightsData.push(...data);
+        await driver.findElement(By.xpath("//button[span[contains(text(), 'Outras companhias')]]")).click();
+        data = await scrollAndSearch(driver);
+        flightsData.push(...data);
+
+
+        console.log(flightsData.length,"nesta pagina")
+        //flightsData.push("Dia"+count,...data);
+        fs.writeFileSync(`voos_dia${count}.json`, JSON.stringify(flightsData, null, 2), "utf-8");
+        count ++;
+        console.log("passando para o próximo dia..")
+        await passDay(driver);
+    }
+    
+    
+})();
+
+//remove a data em milissegundos da url e adiciona um dia
+const passDay = async (driver)=>{
+
+    let nextDay = 86400000;
+    let urlFLigthPage = await driver.getCurrentUrl();
+    let objectUrl = new URL(urlFLigthPage);
+    let departureDate = objectUrl.searchParams.get("departureDate");
+    const date = parseInt(departureDate) + nextDay;
+    await driver.get(`https://www.smiles.com.br/mfe/emissao-passagem/?adults=1&cabin=ALL&children=0&departureDate=${date}&infants=0&isElegible=false&isFlexibleDateChecked=false&returnDate=&searchType=g3&segments=1&tripType=2&originAirport=GRU&originCity=&originCountry=&originAirportIsAny=false&destinationAirport=MIA&destinCity=&destinCountry=&destinAirportIsAny=false&novo-resultado-voos=true`);
+    
+
+}
+
+
+const scrollAndSearch = async (driver) =>{
+    while(true){
+        try{               
+            const moreFlights = await driver.findElement(By.id('SelectFlightList-ida-more'));
+            await driver.executeScript("window.scrollBy(0, window.innerHeight);");
+            await driver.wait(until.elementIsVisible(moreFlights), 5000);
+                await driver.wait(until.elementIsEnabled(moreFlights), 5000);
+            await moreFlights.click();
+            console.log("Carregando mais voos...");
+            await driver.sleep(3000);
+        }catch(error){
+            console.log("todos os voos foram carregados");
+            break;
+        }
+        
+    }
+
+     while(true){
+            try{               
+                const moreFlights = await driver.findElement(By.id('SelectFlightList-ida-more'));
+                await driver.executeScript("window.scrollBy(0, window.innerHeight);");
+                await driver.wait(until.elementIsVisible(moreFlights), 5000);
+                 await driver.wait(until.elementIsEnabled(moreFlights), 5000);
+                await moreFlights.click();
+                console.log("Carregando mais voos...");
+                await driver.sleep(3000);
             }catch(error){
-                console.log("botão não encontrado");
+                console.log("todos os voos foram carregados");
                 break;
             }
             
         }
 
         const flights = await driver.findElements(By.css('div[data-testid="openAccordion"]'))
-
+        console.log("Examinando voos")
         const data = await driver.executeScript(function (...flights) {
             return flights.map(flight => {
 
@@ -126,25 +182,5 @@ const fs = require("fs");
             });
         }, ...flights);
 
-        flightsData.push("Página"+count,...data);
-        count ++;
-        await passDay(driver);
-    }
-    
-
-    fs.writeFileSync("voos_data.json", JSON.stringify(flightsData, null, 2), "utf-8");
-
-})();
-
-//remove a data em milissegundos da url e adiciona um dia
-const passDay = async (driver)=>{
-
-    let nextDay = 86400000;
-    let urlFLigthPage = await driver.getCurrentUrl();
-    let objectUrl = new URL(urlFLigthPage);
-    let departureDate = objectUrl.searchParams.get("departureDate");
-    const date = parseInt(departureDate) + nextDay;
-    await driver.get(`https://www.smiles.com.br/mfe/emissao-passagem/?adults=1&cabin=ALL&children=0&departureDate=${date}&infants=0&isElegible=false&isFlexibleDateChecked=false&returnDate=&searchType=g3&segments=1&tripType=2&originAirport=GRU&originCity=&originCountry=&originAirportIsAny=false&destinationAirport=MIA&destinCity=&destinCountry=&destinAirportIsAny=false&novo-resultado-voos=true`);
-    
-
+        return data;
 }
